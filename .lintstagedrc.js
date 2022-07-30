7969bb0aa5be7ae422ad605a06807e8fd2554f85
rndy28 <rendyramadhan838@gmail.com>
2022-07-30 12:54:05 +0700

    add pre-commit hook

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,14 @@
-const path = require("path");
-
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(" --file ")}`;
-
 module.exports = {
-  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  // this will check Typescript files
+  "**/*.(ts|tsx)": () => "yarn tsc --noEmit",
+
+  // This will lint and format TypeScript and
+  //JavaScript files
+  "**/*.(ts|tsx|js)": (filenames) => [
+    `yarn eslint --fix ${filenames.join(" ")}`,
+    `yarn prettier --write ${filenames.join(" ")}`,
+  ],
+
+  // this will Format MarkDown and JSON
+  "**/*.(md|json)": (filenames) => `yarn prettier --write ${filenames.join(" ")}`,
 };
